9dc5cd33de81fc1bcb45c75562b2511045e4e952
Juan Vivanco <juansebastianvc@gmail.com>
2021-12-19 04:54:00 -0500

    Remove album img on episodes description

--- src/components/PodcastDetailsEpisodes.jsx
+++ src/components/PodcastDetailsEpisodes.jsx
@@ -25,11 +25,6 @@ const PodcastDetailsEpisodes = ({ episodes, handlePause, handlePlay }) => {
                 className='flex w-full max-w-full mx-auto overflow-hidden 
                             hover:bg-gray-900 border-b border-gray-800
                              shadow-md px-4  py-6'>
-                <img
-                  className='object-cover w-32 h-32 rounded pt-5 md:pt-1'
-                  alt='User avatar'
-                  src={episode.artworkUrl160} />
-
                 <div className='flex items-center'>
                   <div className='pl-3'>
                     <div className=' dark:text-gray-200'>
