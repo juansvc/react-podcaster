cf539f28e0587d06285f6d8a01bc03da8f498ab2
Juan Vivanco <juansebastianvc@gmail.com>
2021-12-19 04:45:05 -0500

    Update search and details screen

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,11 +1,115 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDebounce } from 'use-debounce';
+import axios from 'axios';
 
-const SearchScreen = () => {
+import SearchResultContainer from '../containers/SearchResult/SearchResultContainer'
+
+import search_icon_black from '../icons/search_icon_black.svg'
+import cancel_close_delete_icon from '../icons/cancel_close_delete_icon.svg'
+
+
+import { BASE_URL } from '../utils/consts'
+
+const initalText = ' ';
+
+function SearchScreen() {
+  const [searchText, setSearchText] = useState(initalText)
+  const [searchPodcastResults, setSearchPodcastResults] = useState({})
+  const [searchEpisodeResults, setSearchEpisodeResults] = useState({})
+  const [debouncedText] = useDebounce(searchText, 500)
+  const [activeSearchText, setActiveSearchText] = useState('')
+
+  useEffect(() => {
+    const source = axios.CancelToken.source()
+
+    if (debouncedText) {
+      getPodcasts(debouncedText)
+        .then(data => {
+          setSearchPodcastResults(data)
+        })
+        .catch(err => console.log(err))
+
+      getEpisodes(debouncedText)
+        .then(data => {
+          setSearchEpisodeResults(data)
+        })
+        .catch(err => console.log(err))
+    }
+    else {
+      setSearchPodcastResults({})
+      setSearchEpisodeResults({})
+    }
+
+    return () => {
+      source.cancel(
+        'Component unmounted or 24 hours changed'
+      )
+    }
+  }, [debouncedText, searchText])
+
+  const handleSearchCancel = (e) => {
+    setActiveSearchText('')
+    setSearchText('')
+    e.target.value = ''
+  }
+
+  const { resultCount: podcastResultCount, results: podcastResults } = searchPodcastResults
+  const { results: episodeResults } = searchEpisodeResults
+
   return (
-    <div>
-      <h1 className="text-white">SearchScreen</h1>
-    </div>
+    <section>
+      <div className='container px-5 mx-auto'>
+        <div className='flex flex-row justify-center mt-5 p-5 '>
+          <div className='relative w-full md:w-4/12'>
+            <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
+              <img src={search_icon_black} alt='search_icon_black' />
+            </span>
+            <input
+              type='text'
+              className='w-full py-3 pl-10 pr-4 text-gray-900 bg-white border border-gray-300 rounded-full'
+              placeholder='Podcast'
+              aria-label='Podcast'
+              value={searchText}
+              onChange={(e) => {
+                setSearchText(e.target.value)
+                setActiveSearchText(e.target.value)
+              }} />
+            {
+              activeSearchText && 
+              <span className='absolute inset-y-0 right-0 flex items-center pr-2 cursor-pointer'>
+                <img 
+                  src={cancel_close_delete_icon} 
+                  alt='cancel_close_delete_icon'
+                  onClick={(e) => handleSearchCancel(e)} />
+              </span>
+            }
+          </div>
+        </div>
+        {
+          activeSearchText !== ''
+            ?
+            <SearchResultContainer
+              podcastResultCount={podcastResultCount}
+              podcastResults={podcastResults}
+              episodeResults={episodeResults}
+              activeSearchText={activeSearchText} />
+            :
+            <div className='text-white'>Please type something to start searching.</div>
+        }
+      </div>
+
+    </section>
   )
+}
+
+const getPodcasts = async (text) => {
+  const response = await axios.get(`${BASE_URL}search?term=${text}&entity=podcast`)
+  return response.data
+}
+
+const getEpisodes = async (text) => {
+  const response = await axios.get(`${BASE_URL}search?term=${text}&entity=podcastEpisode&limit=4`)
+  return response.data
 }
 
 export default SearchScreen
