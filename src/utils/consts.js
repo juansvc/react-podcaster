8b432630f16e14f8bc21f30f34fbbaee02e5630b
juansvc <juansebastianvc@gmail.com>
2022-11-27 20:53:50 -0500

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,7 +1,7 @@
 const CORS_URL = 'https://cors-anywhere.herokuapp.com/' // Enables cross-origin requests to anywhere
 const APPLE_API_URL = 'https://itunes.apple.com/'
 
-export const BASE_URL = String(CORS_URL + APPLE_API_URL)
+export const BASE_URL = String(APPLE_API_URL)
 
 // export const HOMESCREEN_API_URL = String(`${BASE_URL}us/rss/toppodcasts/limit=100/genre=1310/json`)
 export const HOMESCREEN_API_URL = String(`${BASE_URL}search?term=podcasts&genreId=1310&limit=100`)
